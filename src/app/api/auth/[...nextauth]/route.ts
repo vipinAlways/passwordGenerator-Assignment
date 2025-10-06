8a14fbed64d3cfb0ecc0758a488e0261b4b55679
Your New Name <vipinkumarti555@gmail.com>
2025-10-06 21:46:26 +0530

    first commit

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,12 @@
+import NextAuth from "next-auth";
+import { cache } from "react";
+
+import { authConfig } from "./config";
+
+const { auth: uncachedAuth, handlers, signIn, signOut } = NextAuth(authConfig);
+
+const auth = cache(uncachedAuth);
+
+export { auth, handlers, signIn, signOut };
+
+export const { GET, POST } = handlers;
